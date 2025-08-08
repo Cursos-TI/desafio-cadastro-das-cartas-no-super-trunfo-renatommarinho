264d34285c8c80d63949fd889a4d681fc278c1bb
Renato Marinho <renatommarinho@gmail.com>
2025-08-08 19:53:32 +0000

    novo commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,9 +1,91 @@
 #include <stdio.h>
     //Atividade feita por Renato Marinho
-    
-    printf("Desafio Carta Super Trunfo - Novato\n");
+
+   
 
 int main() {
-   
+    printf("Desafio Carta Super Trunfo - Novato\n");
+
+    //Primeira carta
+    char estado1[2];
+    char codigo1[50];
+    char cidade1[50];
+    int populacao1;
+    float area1;
+    float pib1;
+    int pontos1;
+
+    printf("Digite o Estado (uma letra A-H): \n");
+    scanf("%s", &estado1);
+
+    printf("Digite o Código da Carta: \n");
+    scanf("%s", &codigo1);
+
+    printf("Digite o Nome da Cidade: \n");
+    scanf(" %[^\n]", &cidade1);
+
+    printf("Digite a População: \n");
+    scanf("%d", &populacao1);
+
+    printf("Digite a Área em Km²: \n");
+    scanf("%f", &area1);
+
+    printf("Digite o PIB da Cidade: \n");
+    scanf("%f", &pib1);
+
+    printf("Digite o Número de Pontos Turísticos: \n");
+    scanf("%d", &pontos1);
+
+    // Agora a saída pulando linha e agrupado da carta 1
+    printf("\nCarta 1:\n");
+    printf("Estado: %s\n", estado1);
+    printf("Código: %s\n", codigo1);
+    printf("Nome da Cidade: %s\n", cidade1);
+    printf("População: %d\n", populacao1);
+    printf("Área: %f km²\n", area1);
+    printf("PIB %f bilhões de reais\n", pib1);
+    printf("Número de Pontos Turísticos: %d\n", pontos1);
+
+
+    //Segunda Carta
+    char estado2[2];
+    char codigo2[50];
+    char cidade2[50];
+    int populacao2;
+    float area2;
+    float pib2;
+    int pontos2;
+
+    printf("Digite o Estado (uma letra A-H): \n");
+    scanf("%s", &estado2);
+
+    printf("Digite o Código da Carta: \n");
+    scanf("%s", &codigo2);
+
+    printf("Digite o Nome da Cidade: \n");
+    scanf(" %[^\n]", &cidade2);
+
+    printf("Digite a População: \n");
+    scanf("%d", &populacao2);
+
+    printf("Digite a Área em Km²: \n");
+    scanf("%f", &area2);
+
+    printf("Digite o PIB da Cidade: \n");
+    scanf("%f", &pib2);
+
+    printf("Digite o Número de Pontos Turísticos: \n");
+    scanf("%d", &pontos2);
+
+    // Agora a saída pulando linha e agrupado da carta 2
+    printf("\nCarta 2:\n");
+    printf("Estado: %s\n", estado2);
+    printf("Código: %s\n", codigo2);
+    printf("Nome da Cidade: %s\n", cidade2);
+    printf("População: %d\n", populacao2);
+    printf("Área: %f km²\n", area2);
+    printf("PIB %f bilhões de reais\n", pib2);
+    printf("Número de Pontos Turísticos: %d\n", pontos2);
+
     return 0;
 }
